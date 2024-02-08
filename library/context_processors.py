4b07b80e54909ae5a8832c40649a6dbeb25eddbe
hellen-22 <hellenwain@gmail.com>
2024-02-08 09:44:21 +0300

    clean up

---
+++ library/context_processors.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+def greeting(request):
+    current_time = datetime.now().time()
+    if current_time.hour < 12:
+        greeting = "Morning"
+    elif current_time.hour < 16:
+        greeting = "Afternoon"
+    else:
+        greeting = "Evening"
+
+    return {"greeting": greeting}
